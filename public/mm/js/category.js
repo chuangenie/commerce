53c380858a0e3baf5b0e6c3414aad04d307b8a6a
NG <15271906963@163.com>
2018-10-21 22:21:03 +0800

    分类页静态搭建完成

---
+++ public/mm/js/category.js
@@ -0,0 +1,36 @@
+/**
+ * Created by 97865 on 2018/10/21.
+ */
+$(function () {
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
