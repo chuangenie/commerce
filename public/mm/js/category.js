e9b80afd7ade51e74d69c8448b56885221f40326
NG <15271906963@163.com>
2018-10-22 15:53:23 +0800

    分类页接近完成

--- public/mm/js/category.js
+++ public/mm/js/category.js
@@ -6,7 +6,13 @@ $(function () {
         deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     });
 
-
+    $.ajax({
+        url: "/category/queryTopCategory",
+        type: "get",
+        success: function(response){
+            console.log(response);
+        }
+    })
 
 
 
